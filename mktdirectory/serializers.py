9d57ae658f390e7aaad3560d1342800122b490d9
Biola <52200708+decorouz@users.noreply.github.com>
2022-09-25 00:29:35 +0100

    Create serializer

---
+++ mktdirectory/serializers.py
@@ -0,0 +1,16 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Market
+
+
+class MarketDirectorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Market
+        fields = (
+            "market_code",
+            "name",
+            "brief_details",
+            "num_vendors",
+            "market_schedule",
+            "reference_mkt_date",
+        )
