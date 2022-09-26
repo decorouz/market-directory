696268f94aa04d23fcd9b615c8705d2f7d8b6f06
Biola <52200708+decorouz@users.noreply.github.com>
2022-09-26 23:59:58 +0100

    add tag app

---
+++ tag/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type of an object (Product, Market, Video, Article, News). We can find the Table
+    # ID of the object. Find any records
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        indexes = [models.Index(fields=["content_type", "object_id"])]
