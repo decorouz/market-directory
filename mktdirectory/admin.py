6c2a9c99b597b552d901148cc87d95b5cba11fa7
Biola <52200708+decorouz@users.noreply.github.com>
2022-09-29 13:24:04 +0100

    computed columns

--- mktdirectory/admin.py
+++ mktdirectory/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html
+from django.utils.http import urlencode
+
+from django.urls import reverse
 from .models import (
     Commodity,
     Category,
@@ -7,18 +12,14 @@ from .models import (
     AcceptedPaymentMethod,
     MarketDay,
 )
+from datetime import date, timedelta
 
 
 @admin.register(Commodity)
 class CommodityAdmin(admin.ModelAdmin):
-    list_display = ["name", "overview"]
+    list_display = ("name", "overview", "category")
     list_per_page: int = 10
-
-
-@admin.register(ContactPerson)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "email", "phone")
-    list_per_page = 10
+    list_editable = ("category",)
 
 
 @admin.register(AcceptedPaymentMethod)
@@ -27,8 +28,92 @@ class AcceptablePaymentMethodAdmin(admin.ModelAdmin):
     list_editable = ("charges",)
 
 
-admin.site.register(Market)
-admin.site.register(MarketDay)
+@admin.register(MarketDay)
+class MarketDayAdmin(admin.ModelAdmin):
+    list_display = (
+        "market_date",
+        "market",
+        "commodity",
+        "grade",
+        "commodity_price",
+    )
+    list_select_related = ("market", "commodity")
+    list_editable = ("grade", "commodity_price")
 
 
-admin.site.register(Category)
+@admin.register(Market)
+class MarketAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "brief_details",
+        "num_vendors",
+        "contact_person",
+        "market_days_interval",
+        "reference_mkt_date",
+        "next_market_date",
+    )
+    list_select_related = ("contact_person",)
+    list_per_page = 5
+
+    @admin.display(ordering="reference_mkt_date")
+    def next_market_date(self, market):
+        today = date.today()
+        ref_date = market.reference_mkt_date
+        interval = market.market_days_interval
+
+        while ref_date <= today:
+            ref_date = ref_date + timedelta(interval)
+        return ref_date
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "commodities_count")
+
+    @admin.display(ordering="commodities_count")
+    def commodities_count(self, category):
+        url = (
+            reverse("admin:mktdirectory_commodity_changelist")
+            + "?"
+            + urlencode({"category__id": str(category.id)})
+        )
+        return format_html(
+            "<a href='{}'>{}</a>", url, category.commodities_count
+        )
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .annotate(commodities_count=Count("commodity"))
+        )
+
+
+@admin.register(ContactPerson)
+class ContactPersonAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "market_count",
+    )
+    list_per_page = 10
+
+    @admin.display(ordering="market_count")
+    def market_count(self, contact_person):
+        url = (
+            reverse("admin:mktdirectory_market_changelist")
+            + "?"
+            + urlencode({"contact_person_id": str(contact_person.id)})
+        )
+        return format_html(
+            "<a href='{}'>{}</a>", url, contact_person.market_count
+        )
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .annotate(market_count=Count("market"))
+        )
