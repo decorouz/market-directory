89fd1bd1060003cdc015a7a399fd655a3d51cfa5
Biola <52200708+decorouz@users.noreply.github.com>
2022-09-29 13:26:07 +0100

    change admin site header and title

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Market Directory Admin"
+admin.site.index_title = "Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
