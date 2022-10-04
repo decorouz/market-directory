bfac1bddc5017fae4590e6c4bf81ee6b9fb0cb57
Biola <52200708+decorouz@users.noreply.github.com>
2022-10-04 02:06:13 +0100

    Commodity list and detail view

--- mktdirectory/views.py
+++ mktdirectory/views.py
@@ -1,26 +1,44 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
-from mktdirectory.models import Commodity, Market, MarketDay
+from mktdirectory.models import Commodity, Market
 from mktdirectory.serializers import CommoditySerializer, MarketSerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def commodity_list(request):
     """
     List all commodities
     """
-    commodities = Commodity.objects.select_related("category").all()
-    serializer = CommoditySerializer(commodities, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        commodities = Commodity.objects.select_related("category").all()
+        serializer = CommoditySerializer(commodities, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        """create a commodity"""
+        serializer = CommoditySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(["GET", "PUT", "PATCH", "DELETE"])
 def commodity_detail(request, pk):
+
     commodity = get_object_or_404(Commodity, pk=pk)
-    serializer = CommoditySerializer(commodity)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = CommoditySerializer(commodity)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CommoditySerializer(commodity, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "DELETE":
+        commodity.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
