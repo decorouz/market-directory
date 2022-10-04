b9b529d30f17f3bef5b5f2e853304e866bcdd221
Biola <52200708+decorouz@users.noreply.github.com>
2022-10-04 11:34:10 +0100

    Change Category view from FBV to GCBV

--- mktdirectory/urls.py
+++ mktdirectory/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("commodities/<int:pk>/", views.commodity_detail),
     path("markets/", views.market_list),
     path("markets/<int:pk>/", views.market_detail),
-    path("categories/", views.category_list),
-    path("categories/<int:pk>/", views.category_detail),
+    path("categories/", views.CategoryList.as_view()),
+    path("categories/<int:pk>/", views.CategoryDetail.as_view()),
 ]
--- mktdirectory/views.py
+++ mktdirectory/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from rest_framework import status
 from django.db.models import Count
 
@@ -12,38 +13,48 @@ from mktdirectory.serializers import (
 )
 
 
-@api_view(["GET", "POST"])
-def category_list(request):
-    if request.method == "GET":
+class CategoryList(APIView):
+    def get(self, request):
         query_set = Category.objects.annotate(
             commodities_count=Count("commodity")
         ).all()
-        serializer = CategorySerializer(query_set, many=True)
+        serializer = CategorySerializer(
+            query_set, many=True, context={"request": request}
+        )
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET", "PATCH", "DELETE"])
-def category_detail(request, pk):
-    query_set = Category.objects.annotate(commodities_count=Count("commodity"))
-    category = get_object_or_404(query_set, pk=pk)
-    if request.method == "GET":
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        query_set = Category.objects.annotate(
+            commodities_count=Count("commodity")
+        )
+        return get_object_or_404(query_set, pk=pk)
+
+    def get(self, request, pk):
+        category = self.get_object(pk)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
-    elif request.method == "PATCH":
+
+    def patch(self, request, pk):
+        category = self.get_object(pk)
         serializer = CategorySerializer(category, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "DELETE":
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
         if category.commodity_set.count() > 0:
             return Response(
                 {
-                    "error": "Category cannot be deleted because it is associated with one or more commodity"
+                    "error": "Category can not be deleted because it is associated with one or more Commodity"
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
