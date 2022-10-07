49092abd7d9017c57348021776682ef84ee36717
Biola <52200708+decorouz@users.noreply.github.com>
2022-10-07 10:58:36 +0100

    update fbv to cbv

--- mktdirectory/urls.py
+++ mktdirectory/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from mktdirectory import views
 
 urlpatterns = [
-    path("commodities/", views.commodity_list),
-    path("commodities/<int:pk>/", views.commodity_detail),
-    path("markets/", views.market_list),
-    path("markets/<int:pk>/", views.market_detail),
+    path("commodities/", views.CommodityList.as_view()),
+    path("commodities/<int:pk>/", views.CommodityDetail.as_view()),
+    path("markets/", views.MarketList.as_view()),
+    path("markets/<int:pk>/", views.MarketDetail.as_view()),
     path("categories/", views.CategoryList.as_view()),
     path("categories/<int:pk>/", views.CategoryDetail.as_view()),
 ]
