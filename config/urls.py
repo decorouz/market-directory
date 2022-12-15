bef4ad111cb764b59e13aef7ec6a3afb320e4f60
Biola <52200708+decorouz@users.noreply.github.com>
2022-12-15 08:00:47 +0100

    static configurtion

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 admin.site.site_header = "Market Directory Admin"
 admin.site.index_title = "Admin"
 
+
+# use namespacing
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("playground/", include("playground.urls")),
     path("api/", include("markets.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
